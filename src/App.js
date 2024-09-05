d46845b08f2c91c5454679ea6bd9e444c721888c
mesay <mmekonen@chegebeya.com>
2024-09-05 13:50:45 +0300

    List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+  
+function App(props) {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const user = {
+      name:'John',
+      imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+      age:25
+    }
+    
+    const products = [
+      { title: 'Cabbage', id: 1 },
+      { title: 'Garlic', id: 2 },
+      { title: 'Apple', id: 3 },
+    ];
+
+
+   
+    
+const listItems = products.filter(product => 
+  product.title.endsWith('c'))
+  .map(product => 
+    <li key={product.id}>
+      
+      {product.title}
+    </li>
+    )
+
+
+
+
+  return(
+    <div className='container'>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+      {listItems}
     </div>
   );
 }
